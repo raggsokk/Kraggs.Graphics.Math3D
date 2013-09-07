6599eab36f3f6ef5cfff11427bbb3c03b14503b8
Jarle Hansen <jarle.hansen@gmail.com>
2013-09-07 12:14:11 +0200

    Initial Commit

---
+++ Kraggs.Graphics.Math3D/Kraggs.Graphics.Math3D/MathFunctions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace Kraggs.Graphics.Math3D
+{
+    [DebuggerNonUserCode()]
+    public static class MathFunctions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Pow(float x, float y)
+        {
+            return (float)Math.Pow(x, y);
+        }
+
+        /// <summary>
+        /// Returns the natural exponentiation of x, i.e, e ^ x
+        /// </summary>
+        /// <param name="x">x can be in range [inf-, inf+].</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Exp(float x)
+        {            
+            return (float)Math.Exp(x);
+        }
+
+        /// <summary>
+        /// Returns the natural logarithm of x, i.e.,
+        /// returns the value y withc satisfies the equation x = e ^ y
+        /// </summary>
+        /// <param name="x">result are undefined if x <= 0</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Log(float x)
+        {            
+            return (float)Math.Log(x);
+        }
+
+        /// <summary>
+        /// Returns 2 raised to the x power.
+        /// </summary>
+        /// <param name="x">can be in range [inf-, inf+].</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Exp2(float x)
+        {
+            //return (float)Math.Pow(2.0d, x);
+            return (float)Math.Exp(0.69314718055994530941723212145818f * x);
+        }
+
+        /// <summary>
+        /// Returns the base 2 log of x, returns the value y,
+        /// with satisfies the equation x = 2 ^ y.
+        /// </summary>
+        /// <param name="x">x needs to be in range [0, inf+] for return to be valid.</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Log2(float x)
+        {
+            Debug.Assert(x >= 0.0f);
+            //return (float)Math.Log(a, newbase);            
+
+            //return (float)Math.Log(x, 2.0d);
+            return (float)(Math.Log(x) / 0.69314718055994530941723212145818f);
+        }
+
+        /// <summary>
+        /// Returns the positive square root of f.
+        /// </summary>
+        /// <param name="f">sqrt function is defined for input values of f in the range [0, inf+].</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Sqrt(float f)
+        {
+            return (float)Math.Sqrt(f);
+        }
+
+        /// <summary>
+        /// Returns the reciprocal of the positive square root of f.
+        /// </summary>
+        /// <param name="f">InverseSqrt function is defined for input values of f in the range [0, inf+].</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float InverseSqrt(float f)
+        {
+            return (float)1.0f / Sqrt(f);
+        }
+    }
+}
---
+++ Progress.md
@@ -0,0 +1,125 @@
+
+** Kraggs.Graphics.Math3D
+
+Milestone 1
+	MathFunctions
+		pow(float,float)
+		exp(float)
+		log(float)
+		exp2(float)
+		log2(float)	
+		sqrt
+		inversesqrt
+	Vec2f
+	Vec3f
+	Vec4f
+		lengthsquared
+		length		
+		normalize
+		distance
+
+Milestone 2
+	Vector (Vec,Vec), (Vec, float)
+		Add 
+		Subtract
+		Multiply
+		Divide
+		Negate
+		Dot
+		Cross
+		Faceforward
+		Reflect
+		Refract
+
+Milestone 3
+	Vec2f
+	Vec3f
+	Vec4f
+		ToString
+		GetHashCode
+		operator == !=
+		operator (Vec,float) + - * /
+		operator (float,Vec) + - * /
+		operator (Vec,Vec)   + - * /
+		Equals
+
+Milestone 4
+	Mat2f
+	Mat3f
+	Mat4f
+		Transpose
+		operator (Mat,Mat) *
+	Matrix
+		Multiply (Mat,Mat)	
+		Determinant
+		inverse
+
+Milestone 5
+	Mat2f
+	Mat3f
+	Mat4f
+		ToString
+		GetHashCode
+		operator == !=
+		Equals		
+
+Milestone 6
+	Matrix4f		
+		ortho
+		lookat
+		frustrum
+		perspective
+		translatexyz
+		rotatex
+		rotatey
+		rotatez
+		rotatexyz
+		scalexyz
+
+Milestone 7
+	MathFunctions
+		clamp (float)
+		Mix (float)
+		Step (float)
+		SmoothStep (float)
+
+	Vector
+		Abs(Vec)
+		Ceiling(Vec)
+		Max(Vec,Vec)
+		Min(Vec,Vec)
+		Floor(Vec)
+		Truncate(Vec)
+		Clamp(Vec)
+		Mix(Vec,Vec)
+		Step(Vec)
+		SmoothStep(Vec)
+
+Milestone 8
+	IVector
+		BaseType
+		GLUniformType
+		GLAttributeType
+	IMatrix
+		BaseType
+		GLUniformType
+		GLAttributeType
+	Stream Extension
+		ReadVec<T>
+		ReadMat<T>
+
+
+
+
+Release 1
+	Milestone 1
+	Milestone 2
+	Milestone 3
+	glm verify milestone 1,2 & 3
+Release 2
+	Milestone 4
+	Milestone 5
+	Milestone 6
+Release 3
+	Milestone 7
+	Milestone 8
